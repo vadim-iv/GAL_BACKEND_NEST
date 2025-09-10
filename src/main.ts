189ba65d5c90e8f5bb2ebc6c29d1af8d5abb13b0
VadimOMEGA <vadimivanov.0320@gmail.com>
2025-09-10 13:23:04 +0300

    Changed backend origin in CORS config

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000', 'https://gal-liard.vercel.app'],
+		origin: ['http://localhost:3000', 'https://gal-backend-six.vercel.app'],
 		credentials: true,
 		exposedHeaders: ['set-cookie'],
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
